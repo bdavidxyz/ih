de5ca0c7f1ff4ab989f0789980098ab79c88961b
David Boureau <bdavidxyz@gmail.com>
2020-06-29 12:28:56 +0200

    now jquery and styles ok

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,10 +5,16 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/activestorage").start()
+// require("channels")
 
-import './bootstrap_custom.js'
+import './bootstrap_custom'
+import '../stylesheets/application'
+
+document.addEventListener('turbolinks:load', () => {
+  console.log(`jQuery version is ${$.fn.jquery}`)
+})
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
