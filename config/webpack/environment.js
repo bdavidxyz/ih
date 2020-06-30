16218462fb58b438c48387f594b16ffedf730ca7
David Boureau <bdavidxyz@gmail.com>
2020-06-30 10:36:20 +0200

    added tree-shaking

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -12,4 +12,48 @@ environment.plugins.append(
   })
 )
 
+
+environment.generateMultiWebpackConfig = function(env) {
+  // Side-Effect: broken manifest.json file will be generated if
+  // writeToFileEmit enabled, failing the parsing and Webpack 
+  // compilation randomly.
+  // Github Issue: https://github.com/rails/webpacker/issues/1251 
+  env.plugins.get('Manifest').opts.writeToFileEmit = false
+  
+  let webpackConfig = env.toWebpackConfig()
+  
+  // extract entries to map later in order to generate separate 
+  // webpack configuration for each entry.
+  // P.S. extremely important step for tree-shaking
+  let entries = Object.keys(webpackConfig.entry)
+  
+  // Generate a seed file containing all the entries to write to 
+  // manifest.json when writeToFileEmit is enabled for the last 
+  // entry later on. Without it, only last entry will be written   
+  // down to manifest.json
+  environment.plugins.get('Manifest').opts.reduce = function(_, file) {
+    environment.plugins.get('Manifest').opts.seed = Object.assign(
+      environment.plugins.get('Manifest').opts.seed || {},
+      {[file.name] : file.path}
+    )
+    return environment.plugins.get('Manifest').opts.seed
+  }
+  // Finally, map over extracted entries to generate a separate 
+  // Webpack configuration for each entry and enable writeToFileEmit 
+  // only for the last entry
+  return entries.map(function (entryName, i) { 
+    if (i === entries.length - 1) {
+      env.plugins.get('Manifest').opts.writeToFileEmit = true
+      webpackConfig = env.toWebpackConfig()
+    }
+    return Object.assign(
+      {},
+      webpackConfig,
+      { entry: { [entryName] : webpackConfig.entry[entryName] } }
+    )
+  })
+}
+
+
 module.exports = environment
+
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+// disabled for tree-shaking, and replaced by line below
 module.exports = environment.toWebpackConfig()
+// module.exports = environment.generateMultiWebpackConfig(environment)
